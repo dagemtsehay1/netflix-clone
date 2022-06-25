9bbbda6933a91375f7335789a62d45094c336242
dagem <dagemtsehay4@gmail.com>
2022-06-25 10:02:17 -0400

    realese files

---
+++ precache-manifest.5742e337d5794ce8f0722db4446231ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3c6a9eac05d59d680a491cd3840d26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "119bdd28ecfa15c78e5b",
+    "url": "./static/css/main.6b2160e6.chunk.css"
+  },
+  {
+    "revision": "746a7a301999b4a05a87",
+    "url": "./static/js/2.59a31684.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.59a31684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119bdd28ecfa15c78e5b",
+    "url": "./static/js/main.91e19f92.chunk.js"
+  },
+  {
+    "revision": "5cca2ffc9a972715b7dd",
+    "url": "./static/js/runtime-main.d412f870.js"
+  },
+  {
+    "revision": "f42807c11b0bd8a82d141e99232e8a1d",
+    "url": "./static/media/icon-amazon.f42807c1.png"
+  },
+  {
+    "revision": "262e5be1ba67422687dc70b101d0291a",
+    "url": "./static/media/icon-netflix.262e5be1.png"
+  }
+]);
